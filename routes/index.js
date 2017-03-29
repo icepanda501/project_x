4211fcfc276bb5d304784063f04481a1584c8127
icepanda501 <icepanda501@gmail.com>
2017-03-30 01:26:48 +0700

    add post in index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
+
+router.post('/', function(req, res, next) {
+	console.log(req.body)
+	res.send({ test: "test" })
+})
 
 module.exports = router;
